b1e051ef2b1608bd44e54f747387a5d2150ac96c
loveyuan2019 <smywsmyw@163.com>
2020-07-30 08:00:59 +0800

    如何添加请求头

---
+++ useragent_demo/spiders/httpbin.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import json
+
+
+class HttpbinSpider(scrapy.Spider):
+    name = 'httpbin'
+    allowed_domains = ['httpbin.org']
+    start_urls = ['http://httpbin.org/user-agent']
+
+    def parse(self, response):
+        user_agent=json.loads(response.text)['user-agent']
+        print("="*80)
+        print(user_agent)
+        print("="*80)
+        yield scrapy.Request(self.start_urls[0],dont_filter=True)
+        # print(response.text)
+        # pass
